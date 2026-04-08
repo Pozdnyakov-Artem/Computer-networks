37e044cb3906fd1d419d5a9f5e7f3d322100ed27
Artem Pozdnyakov <a.pozdnyakov1@g.nsu.ru>
2026-04-09 00:37:51 +0700

    task10

--- task10/1.py
+++ task10/1.py
@@ -9,14 +9,29 @@ def dig(host):
     ipv6 = subprocess.run(['dig', 'AAAA', host, '+short'], capture_output=True, text=True, encoding='cp866')
     return ipv4.stdout, ipv4.returncode, ipv6.stdout, ipv6.returncode
 
-arr = ["google.com", "ya.ru", "e.mail.ru", "chat.deepseek.com","github.com",
-       "youtube.com", "vk.com", "leetcode.com", "classroom.google.com", "www.ozon.ru"]
+def traceroute(ip_v4):
+    route = subprocess.run(['traceroute', ip_v4],capture_output=True,text=True,encoding='cp866')
+    return route.stdout, route.returncode
 
+def traceroute6(ip_v6):
+    route = subprocess.run(['traceroute6', ip_v6],capture_output=True,text=True,encoding='cp866')
+    return route.stdout, route.returncode
+# arr = ["google.com", "ya.ru", "e.mail.ru", "github.com",
+#        "youtube.com", "vk.com", "leetcode.com", "classroom.google.com", "www.ozon.ru"]
+arr = ["google.com"]
 
 for host in arr:
     ipv4, code_ipv4, ipv6, code_ipv6 = dig(host)
-    print(f"v4 for{host}",ipv4)
-    print(f"v6 for{host}", ipv6)
+    # print(f"v4 for{host}",ipv4)
+    # print(f"v6 for{host}", ipv6)
+    if code_ipv4 and ipv4:
+        for ip in ipv4:
+            route, code = traceroute(ip)
+            print(route)
+    if code_ipv6 and ipv6:
+        for ip in ipv6:
+            route, code = traceroute6(ip)
+            print(route)
 
 
 # df = pd.DataFrame(data)
