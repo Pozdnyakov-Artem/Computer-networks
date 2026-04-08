6e3d13c5b90b0cd9a81b9374e1a76302f9a41325
Artem Pozdnyakov <a.pozdnyakov1@g.nsu.ru>
2026-04-09 03:01:27 +0700

    task10

--- task10/1.py
+++ task10/1.py
@@ -1,4 +1,3 @@
-import re
 import subprocess
 
 import pandas as pd
@@ -16,9 +15,8 @@ def traceroute(ip_v4):
 def traceroute6(ip_v6):
     route = subprocess.run(['traceroute6', ip_v6],capture_output=True,text=True,encoding='cp866')
     return route.stdout, route.returncode
-#arr = ["google.com", "ya.ru", "e.mail.ru", "github.com",
-#        "youtube.com", "vk.com", "leetcode.com", "classroom.google.com", "www.>
-arr = ["youtube.com"]
+arr = ["google.com", "youtube.com", "leetcode.com", "classroom.google.com"]
+#arr = ["youtube.com"]
 
 data={
     "домен" : [],
@@ -33,7 +31,7 @@ data={
 
 def add_to_data(domen, ip, ip_version, route):
     # print(len(route))
-    bias = 0 if ip == "v4" else 1
+    bias = 0 if ip_version == "v4" else 1
     route = route.splitlines() #[:-1]
     # print(route)
     for idx, line in enumerate(route):
@@ -41,20 +39,19 @@ def add_to_data(domen, ip, ip_version, route):
             continue
         line = line.split()
         line = [word for word in line if word != "ms"]
-        # print(idx, line)
+        print(idx, line)
         data["домен"].append(domen)
         data["ip_версия"].append(ip_version)
         data["ip для traceroute"].append(ip)
         data["номер узла"].append(line[0])
         data["ip узла"].append(line[1])
-        data["время первого запроса"].append(line[2+bias])
-        data["время второго запроса"].append(line[3+bias])
-        data["время третьего запроса"].append(line[4+bias])
+        data["время первого запроса"].append(line[2 + bias])
+        data["время второго запроса"].append(line[3 + bias])
+        data["время третьего запроса"].append(line[4 + bias])
 
 
 for host in arr:
     ipv4, code_ipv4, ipv6, code_ipv6 = dig(host)
-    ipv4 = ("".join(ipv4)).splitlines()
     ipv6 = ("".join(ipv6)).splitlines()
 
     if not code_ipv4 and ipv4:
@@ -70,7 +67,5 @@ for host in arr:
             if not code:
                 add_to_data(host, ip, "v6", route)
 
-
 df = pd.DataFrame(data)
 df.to_csv("1.csv", index=False)
-
